e829d21eb47fce96e2985723572adfb073cd92f2
Gabriel <phangabriel@gmail.com>
2024-07-14 21:19:04 +0700

    Resolve deprecated functions in SSL and MySQL Connector in Python 3.12 to use SSL certificates.

--- client/pages/all.js
+++ client/pages/all.js
@@ -4,64 +4,64 @@ const quotationsSection = document.getElementById("quotations-section");
 async function processApp() {
 
     await fetchData()
-    .then(response => {
-      serverStatus.innerHTML = "Successfully connected to the database !";
-      displayData(response);
-    })
-    .catch(error => {
-      serverStatus.innerHTML = error.message;
-    });
+        .then(response => {
+            serverStatus.innerHTML = "Successfully connected to the database !";
+            displayData(response);
+        })
+        .catch(error => {
+            serverStatus.innerHTML = error.message;
+        });
 }
 
 async function fetchData() {
-  try {
-    const response = await fetch("http://localhost:5000/api/quotations/get_quotations_with_authors");
-    if (!response.ok) {
-      throw new Error(`Error requesting the server: '${response.status}'.`);
-    }
-    formattedResponse = await response.json();
-    return formattedResponse.data;
+    try {
+        const response = await fetch("https://localhost:5000/api/quotations/get_quotations_with_authors");
+        if (!response.ok) {
+            throw new Error(`Error requesting the server: '${response.status}'.`);
+        }
+        formattedResponse = await response.json();
+        return formattedResponse.data;
 
-  } catch (error) {
-    throw new Error(`Error attempting request the server: '${error.message}'.`);
-  }
+    } catch (error) {
+        throw new Error(`Error attempting request the server: '${error.message}'.`);
+    }
 }
 
 function displayData(data) {
-  for (let row of data) {
-    displayQuotation(row);
-  }
+    for (let row of data) {
+        displayQuotation(row);
+    }
 }
 
 function displayQuotation(data) {
-  const container = document.createElement("div");
-  container.className = "quote-container";
-  
-  const box = document.createElement("div");
-  box.className = "quote-box";
-  
-  const quote = document.createElement("q");
-  quote.className = "left";
-  quote.textContent = data.quotation;
-  
-  const author = document.createElement("p");
-  author.className = "right";
-  
-  if (data.first_name !== null) {
-    author.textContent = `${data.first_name} `;
-  }
-  
-  author.textContent += `${data.last_name}`;
-  
-  if (data.publication !== null) {
-    author.textContent += `, ${data.publication}`;
-  }
-  
-  box.appendChild(quote);
-  box.appendChild(author);
-  
-  container.appendChild(box);
-  quotationsSection.appendChild(container);
+    const container = document.createElement("div");
+    container.className = "quote-container";
+
+    const box = document.createElement("div");
+    box.className = "quote-box";
+
+    const quote = document.createElement("q");
+    quote.className = "left";
+    quote.textContent = data.quotation;
+
+    const author = document.createElement("p");
+    author.className = "right";
+
+    if (data.first_name !== null) {
+        author.textContent = `${data.first_name} `;
+    }
+
+    author.textContent += `${data.last_name}`;
+
+    if (data.publication !== null) {
+        author.textContent += `, ${data.publication}`;
+    }
+
+    box.appendChild(quote);
+    box.appendChild(author);
+
+    container.appendChild(box);
+    quotationsSection.appendChild(container);
 }
 
 processApp()
--- server/server.py
+++ server/server.py
@@ -1,22 +1,41 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS
+import ssl
+from flask import Flask, jsonify, request, redirect
+from flask_cors import CORS, cross_origin
 import mysql.connector
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/api/quotations/*": {"origins": "*"}}, supports_credentials=True)
+
+ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+ssl_context.load_cert_chain(certfile="certificate/quotations_server-cert.pem",
+                            keyfile="certificate/quotations_server-key.pem")
+
 
 # MySQL database credentials
-db = {
+DB = {
     "host": "localhost",
+    "database": "quotations",
     "user": "root",
     "password": "nm6dyr!&rZmr&7q#EFh$",
-    "database": "quotations",
+    "ssl_ca": "certificate/quotations_server-cert.pem",
+    "ssl_disabled": False,
 }
 
+
+@app.before_request
+def before_request():
+    if not request.is_secure:
+        return redirect(request.url.replace("http://", "https://"))
+
+
 @app.route("/api/quotations/get_stats", methods=["GET"])
+@cross_origin()
 def get_stats():
+    connection = None
+    cursor = None
+
     try:
-        connection = mysql.connector.connect(**db)
+        connection = mysql.connector.connect(**DB)
         cursor = connection.cursor(dictionary=True)
 
         query_quotations = "SELECT COUNT(*) FROM quotations;"
@@ -30,21 +49,28 @@ def get_stats():
         count_authors = row_authors["COUNT(*)"]
 
         data = f'{{"quotations_count": "{count_quotations}", "authors_count": "{count_authors}"}}'
-        to_return = jsonify({"data": data})
+        response = jsonify({"data": data})
 
     except mysql.connector.Error as error:
-        to_return = jsonify({"error": error})
+        response = jsonify({"error": str(error)})
 
     finally:
-        cursor.close()
-        connection.close()
+        if cursor is not None:
+            cursor.close()
+        if connection is not None:
+            connection.close()
     
-    return to_return
+    return response
+
 
 @app.route("/api/quotations/get_quotations_with_authors", methods=["GET"])
+@cross_origin()
 def get_quotations_with_authors():
+    connection = None
+    cursor = None
+
     try:
-        connection = mysql.connector.connect(**db)
+        connection = mysql.connector.connect(**DB)
         cursor = connection.cursor(dictionary=True)
 
         query = "SELECT * FROM quotations_with_authors;"
@@ -52,22 +78,28 @@ def get_quotations_with_authors():
 
         data = cursor.fetchall()
 
-        to_return = jsonify({"data": data})
+        response = jsonify({"data": data})
 
     except mysql.connector.Error as error:  
-        to_return = jsonify({"error": error})
-
+        response = jsonify({"error": error})
 
     finally:
-        cursor.close()
-        connection.close()
+        if cursor is not None:
+            cursor.close()
+        if connection is not None:
+            connection.close()
     
-    return to_return
+    return response
+
 
 @app.route("/api/quotations/search_quotations", methods=["GET"])
+@cross_origin()
 def search_quotations():
+    connection = None
+    cursor = None
+
     try:
-        connection = mysql.connector.connect(**db)
+        connection = mysql.connector.connect(**DB)
         cursor = connection.cursor(dictionary=True)
 
         phrase = request.args.get("phrase")
@@ -77,17 +109,20 @@ def search_quotations():
 
         data = cursor.fetchall()
 
-        to_return = jsonify({"data": data})
+        response = jsonify({"data": data})
     
     except mysql.connector.Error as error:
-        to_return = jsonify({"error": error})
+        response = jsonify({"error": error})
         print(error)
 
     finally:
-        cursor.close()
-        connection.close()
+        if cursor is not None:
+            cursor.close()
+        if connection is not None:
+            connection.close()
 
-    return to_return
+    return response
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, ssl_context=ssl_context, port=5000)
